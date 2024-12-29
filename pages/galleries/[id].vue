c17bd235ccd504ddf864cdc712a7487014c37d03
Adrian <adrianmal506@gmail.com>
2024-12-29 17:47:25 +0100

    adding pages, route parameters, nuxtlink

---
+++ pages/galleries/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>Album - {{ id }}</p>
+        
+    </div>
+</template>
+
+<script>
+    const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <header>
+            <nav>
+                <NuxtLink to="/galleries">Galeria albumów</NuxtLink>
+            </nav>
+        </header>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
